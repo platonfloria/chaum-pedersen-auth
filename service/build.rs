35b79f68a2eb6763e0261c2da4831f3b620d4491
Platon Floria <platon.floria@gmail.com>
2023-09-04 17:32:56 +0800

    feat: added protobuf specs

--- service/build.rs
+++ service/build.rs
@@ -1,2 +1,17 @@
+use std::{env, path::PathBuf};
+
+
 fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("descriptor.bin"))
+        .build_server(true)
+        .compile(
+            &[
+                "./zkp_auth.proto",
+            ],
+            &["."],
+        )
+        .unwrap();
 }

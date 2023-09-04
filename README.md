f1abd34d4bcfacc46891b8c66f3cfc1320b8e770
Platon Floria <platon.floria@gmail.com>
2023-09-04 18:09:39 +0800

    feat: added grpc service

--- README.md
+++ README.md
@@ -31,3 +31,39 @@ or
 ```bash
 make run_local
 ```
+
+## Smoke testing
+Install grpcurl to hit api endpoints from the command line.
+https://github.com/fullstorydev/grpcurl#installation
+
+### Register
+```bash
+grpcurl -plaintext \
+    --d '{
+        "user": "testuser",
+        "y1": 0,
+        "y2": 0
+    }' \
+    localhost:50051 zkp_auth.Auth.Register
+```
+
+### Create authentication challenge
+```bash
+grpcurl -plaintext \
+    --d '{
+        "user": "testuser",
+        "r1": 0,
+        "r2": 0
+    }' \
+    localhost:50051 zkp_auth.Auth.CreateAuthenticationChallenge
+```
+
+### Verify authentication
+```bash
+grpcurl -plaintext \
+    --d '{
+        "auth_id": "",
+        "s": 0
+    }' \
+    localhost:50051 zkp_auth.Auth.VerifyAuthentication
+```
--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,59 @@
-fn main() {
-    println!("Hello, world!");
+use eyre::Result;
+use tonic::{transport::Server, Request, Response, Status};
+
+mod pb2 {
+    tonic::include_proto!("zkp_auth");
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("descriptor");
+}
+
+
+#[derive(Debug, Default)]
+pub struct API {}
+
+#[tonic::async_trait]
+impl pb2::auth_server::Auth for API {
+    async fn register(&self, request: Request<pb2::RegisterRequest>) -> Result<Response<pb2::RegisterResponse>, Status> {
+        println!("register");
+        Ok(Response::new(pb2::RegisterResponse {}))
+    }
+
+    async fn create_authentication_challenge(&self, request: Request<pb2::AuthenticationChallengeRequest>) -> Result<Response<pb2::AuthenticationChallengeResponse>, Status> {
+        println!("create_authentication_challenge");
+        Ok(Response::new(pb2::AuthenticationChallengeResponse {
+            auth_id: "auth_id".into(),
+            c: 1,
+        }))
+    }
+
+    async fn verify_authentication(&self, request: Request<pb2::AuthenticationAnswerRequest>) -> Result<Response<pb2::AuthenticationAnswerResponse>, Status> {
+        println!("verify_authentication");
+        Ok(Response::new(pb2::AuthenticationAnswerResponse {
+            session_id: "session_id".into(),
+        }))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::0]:50051".parse()?;
+    let api = API::default();
+
+    let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
+    health_reporter
+        .set_serving::<pb2::auth_server::AuthServer<API>>()
+        .await;
+
+    let reflection_service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(pb2::FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
+
+    Server::builder()
+        .add_service(pb2::auth_server::AuthServer::new(api))
+        .add_service(health_service)
+        .add_service(reflection_service)
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
